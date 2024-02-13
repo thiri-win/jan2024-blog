71d818ef10c22dae9260d8347cc535e915142821
Thiri Win <thiriwinzhuang@gmail.com>
2024-02-13 11:39:37 +0630

    showing all categories list in category-index

--- category-index.php
+++ category-index.php
@@ -1,4 +1,12 @@
-<?php include 'layout/header.php' ?>
+<?php include 'layout/header.php'; ?>
+
+<?php
+
+include 'db.php';
+$query = 'SELECT * FROM categories';
+$result = mysqli_query($conn, $query);
+
+?>
 
 <a href="category-create.php" class="tm-external-link mb-3">New Category</a>
 <table class="table table-sm">
@@ -10,14 +18,20 @@
         </tr>
     </thead>
     <tbody>
-    <tr>
-        <td>1</td>
-        <td>title</td>
-        <td>
-            <a href="" class="text-warning mr-3"><i class="fas fa-edit"></i></a>
-            <a href="" class="text-danger mr-3"><i class="fas fa-trash-alt"></i></a>
-        </td>
-    </tr>
+        <?php foreach($result as $category): ?>
+        <tr>
+            <td><?= $category['id'] ?></td>
+            <td><?= $category['name'] ?></td>
+            <td>
+                <a href="category-edit.php?id=<?= $category['id'] ?>" class="text-warning mr-3">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <a href="category-delete.php?id=<?= $category['id'] ?>" class="text-danger mr-3">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
     </tbody>
 </table>
-<?php include 'layout/footer.php' ?>
+<?php include 'layout/footer.php'; ?>
