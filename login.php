19123c7840df3f44fa52811a4e6ce3120d769c5f
Thiri Win <thiriwinzhuang@gmail.com>
2024-03-12 12:15:45 +0630

    login

--- login.php
+++ login.php
@@ -3,21 +3,21 @@
 session_start();
 include 'db.php';
 
-if(isset($_POST['login'])) {
+if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $error = [];
 
-    empty(trim($email)) ? $error[] = "Email is required" : '';
-    empty(trim($password)) ? $error[] = "Password is required" : '';
+    empty(trim($email)) ? ($error[] = 'Email is required') : '';
+    empty(trim($password)) ? ($error[] = 'Password is required') : '';
 
-    if(!$error) {
+    if (!$error) {
         $password = md5($password);
         $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
         $result = mysqli_query($conn, $query);
-        if($result) {
-            if(mysqli_num_rows($result) == 0) {
-                $error[] = "Email or Password is wrong";
+        if ($result) {
+            if (mysqli_num_rows($result) == 0) {
+                $error[] = 'Email or Password is wrong';
             } else {
                 $_SESSION['user'] = mysqli_fetch_assoc($result);
                 $_SESSION['auth'] = true;
@@ -38,7 +38,7 @@ include 'layout/header.php';
 
 <h1>Login</h1>
 <form action="" method="post">
-    <?php include 'layout/error.php' ?>
+    <?php include 'layout/error.php'; ?>
     <input type="email" placeholder="Your Email" name="email" class="form-control mb-3">
     <input type="password" placeholder="Your Password" name="password" class="form-control mb-3">
     <input type="submit" value="Login" name="login" class="btn btn-success">
